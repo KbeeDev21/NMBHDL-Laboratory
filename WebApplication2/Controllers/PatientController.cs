0cae86ed66228998f227f6ef91ea7cd9dc7a9127
Karabo <pauli@TSHEGOFATSO>
2026-05-19 14:10:22 +0200

    Patient updated

--- WebApplication2/Controllers/PatientController.cs
+++ WebApplication2/Controllers/PatientController.cs
@@ -1,7 +1,39 @@
-﻿namespace WebApplication2.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
 {
     public class PatientController: Controller
     {
+        private readonly ILogger<PatientController> _logger;
 
+        public PatientController(ILogger<PatientController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult ViewTestRequests()
+        {
+            return PartialView("_ViewTestRequests");
+        }
+
+        public IActionResult MedicalHistory()
+        {
+            return PartialView("_MedicalHistory");
+        }
+
+        public IActionResult ConsentManagement()
+        {
+            return PartialView("_ConsentManagement");
+        }
+
+        public IActionResult PatientReports()
+        {
+            return PartialView("_PatientReports");
+        }
+
+        public IActionResult TrackTestResults()
+        {
+            return PartialView("_TrackTestresults");
+        }
     }
 }
