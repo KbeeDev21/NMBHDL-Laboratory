c57c9f59fa9f33abf5fbbe826fa195d1e3d3f25c
KeenenJ <106118912+KeenenJ@users.noreply.github.com>
2026-05-22 14:58:04 +0200

    Implemented Lab Technician dashboard and workflow pages

--- WebApplication2/Controllers/LabTechnicianController.cs
+++ WebApplication2/Controllers/LabTechnicianController.cs
@@ -1,6 +1,75 @@
-﻿namespace WebApplication2.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
 {
-    public class LabTechnicianController
+    public class LabTechnicianController : Controller
     {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult StatUrgent()
+        {
+            return View();
+        }
+
+        public IActionResult VerifyResults()
+        {
+            return View();
+        }
+
+        public IActionResult TestDetail()
+        {
+            return View();
+        }
+
+        public IActionResult Escalations()
+        {
+            return View();
+        }
+
+        public IActionResult Reports()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult EditProfile()
+        {
+            return View();
+        }
+
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+        public IActionResult AwaitingSelection()
+        {
+            return View();
+        }
+        public IActionResult AssignedTests()
+        {
+            return View();
+        }
+        public IActionResult CaptureResults()
+        {
+            return View();
+        }
+        public IActionResult VerificationQueue()
+        {
+            return View();
+        }
+        public IActionResult ReviewQueue()
+        {
+            return View();
+        }
+        public IActionResult ReceiveSamples()
+        {
+            return View();
+        }
     }
 }
